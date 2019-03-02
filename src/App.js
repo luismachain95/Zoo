fe2ef1d409ed980a80ec14d7024fe1e200ccf239
Luis <luismachain95@hotmail.com>
2019-03-02 00:29:27 -0300

    Archivos Iniciales

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Zoo from './Components/Zoo/Zoo';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="content">
+        <h1>Zoo - Simple Zoom (React)</h1>
+        <Zoo image="/image/img.jpg" width='500' height='500'/>
       </div>
     );
   }
