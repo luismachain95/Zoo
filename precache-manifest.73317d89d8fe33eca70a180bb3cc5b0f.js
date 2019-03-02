f0b9c3fb26a46a361b2a28dbcc5e2ad7e98d93de
Luis <luismachain95@hotmail.com>
2019-03-02 00:36:21 -0300

    Updates

---
+++ precache-manifest.73317d89d8fe33eca70a180bb3cc5b0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aabb605abc7eef54db8",
+    "url": "/Zoo/static/js/runtime~main.0aabb605.js"
+  },
+  {
+    "revision": "4ee5da14a6981977c872",
+    "url": "/Zoo/static/js/main.4ee5da14.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Zoo/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "4ee5da14a6981977c872",
+    "url": "/Zoo/static/css/main.4ff2332e.chunk.css"
+  },
+  {
+    "revision": "7a1401bef56faf8a401ae71c0e60b934",
+    "url": "/Zoo/index.html"
+  }
+];
